4b3ec56ef82f36099b8df7ee4e3b582a983a861d
Rahaima-Shakeel <164835046+Rahaima-Shakeel@users.noreply.github.com>
2024-03-27 15:29:06 -0700

    rahi-number-guessing

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+console.log("Welcome to Rahi Number Guessing Game");
+const answer = await inquirer.prompt([{
+        name: "userGuessNumber",
+        type: "number",
+        message: "please guess a  number between 1-5: ",
+    },
+]);
+if (answer.userGuessNumber === randomNumber) {
+    console.log("Congratulations! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number.");
+}
